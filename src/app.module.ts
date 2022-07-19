3e35dc00865517a2dcf86ba59bba2dc654d61571
Manuel Da Pena <65864237+NachoDPP@users.noreply.github.com>
2022-07-19 01:59:21 -0400

    Agora Api & CU Llamar Paciente
    - Se implementó el CU para realizar la llamada al paciente.
    - Se integró el generador de tokens de agora.

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,16 +3,18 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './security/auth/auth.module';
 import { DoctorController } from './doctor/infrastructure/controllers/doctor.controller';
-import config from '../ormconfig';
 import { PatientController } from './patient/infrastructure/controllers/patient.controller';
 import { AppointmentController } from './appointment/infrastructure/controllers/appointment.controller';
 import { MedicalRecordController } from './medical-record/infrastructure/controllers/medical-record.controller';
+import { HttpModule } from '@nestjs/axios';
+import config from '../ormconfig';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRoot(config),
     AuthModule,
+    HttpModule
   ],
   controllers: [DoctorController, PatientController, AppointmentController, MedicalRecordController],
   providers: [],
