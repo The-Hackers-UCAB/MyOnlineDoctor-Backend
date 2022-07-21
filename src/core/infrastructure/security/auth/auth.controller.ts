294a01a1b9b8905d2304f497f2972167a846c8f5
Manuel Da Pena <65864237+NachoDPP@users.noreply.github.com>
2022-07-20 21:46:51 -0400

    Revert "Hotfix Cookies Flutter Web"
    This reverts commit 49936c60c933d598e6b6990b14be78929ed54f56.

--- src/core/infrastructure/security/auth/auth.controller.ts
+++ src/core/infrastructure/security/auth/auth.controller.ts
@@ -1,44 +1,14 @@
-import { HttpService } from '@nestjs/axios';
-import { Body, Controller, Get, InternalServerErrorException, Post, Request, UseGuards } from '@nestjs/common';
+import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
 import { SessionGuard } from './sessions/session.guard';
 import { LocalAuthGuard } from './strategies/local.auth.guard';
-import { tap } from 'rxjs';
 
 /** AuthController: Controller de infraestructura encargado de gestionar las peticiones relacionadas con el módulo de seguridad.*/
 @Controller('auth')
 export class AuthController {
-
-    private readonly httpService: HttpService = new HttpService();
-
     @Post('login')
     @UseGuards(LocalAuthGuard)
     async login(@Request() request): Promise<{ value: string }> {
         return { value: "Sesión iniciada." };
-    }
-
-    @Post('doctor/login')
-    async loginDoctor(@Body() body) {
-        let cookie = "";
-        await this.httpService.post(
-            process.env.CURRENT_URL + "" + '/api/auth/login',
-            body,
-            {
-                headers: {
-                    'Content-Type': 'application/json',
-                }
-            }
-        ).pipe(
-            tap({
-                next: (response) => {
-                    cookie = response?.headers['set-cookie'][0];
-                },
-                error: (error) => {
-                    throw new InternalServerErrorException();
-                }
-            })
-        ).toPromise();
-
-        return cookie;
     }
 
     @Get('logout')
