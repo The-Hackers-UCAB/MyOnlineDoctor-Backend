737444867dc51e53ce20c9846379c2b7644cf356
santiagofv36 <59099144+santiagofv36@users.noreply.github.com>
2022-07-31 14:03:58 -0400

    Serivicios de aplicacion de modificacion de registro medico
    - Se implemento el servicio de aplicacion de modificacion de la descripcion del registro medico
    - Se implemento el servicio de aplicacion de modificacion del diagnostico del registro medico

---
+++ src/medical-record/domain/events/medical-record-description-modified.ts
@@ -0,0 +1,19 @@
+import { DomainEvent } from "src/core/domain/events/domain-event";
+import { MedicalRecordDescription } from "../value-objects/medical-record-description";
+import { MedicalRecordID } from "../value-objects/medical-record-id";
+
+export class MedicalRecordDescriptionModified extends DomainEvent {
+    protected constructor(
+        public id: MedicalRecordID,
+        public description: MedicalRecordDescription
+    ) {
+        super();
+    }
+
+    static create(
+        id: MedicalRecordID,
+        description: MedicalRecordDescription
+    ): MedicalRecordDescriptionModified {
+        return new MedicalRecordDescriptionModified(id, description);
+    }
+}
