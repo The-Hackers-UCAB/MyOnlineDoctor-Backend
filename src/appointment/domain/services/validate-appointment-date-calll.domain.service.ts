15a295eb83601d6ffd88ed098cf127cc56dbfbb9
abadillo <63127054+abadillo@users.noreply.github.com>
2022-08-03 20:18:43 -0400

    Add validacion de inicio de videollamada luego de una hora despues de la hora de pautada

--- src/appointment/domain/services/validate-appointment-date-calll.domain.service.ts
+++ src/appointment/domain/services/validate-appointment-date-calll.domain.service.ts
@@ -3,9 +3,18 @@ import { Appointment } from "../appointment";
 
 export class ValidateAppointmentDateCallDomainService implements IDomainService<Appointment, boolean>{
     execute(dto: Appointment): boolean {
+
         if (dto.Date.Value < (new Date(Date.now()))) {
             return false;
         }
+
+        let extraTime = dto.Date.Value;
+        extraTime.setHours(extraTime.getHours() + dto.Duration.Value + 1);
+
+        if (extraTime > (new Date(Date.now()))) {
+            return false;
+        }
+
         return true;
     }
 }
